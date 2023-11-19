1ba96d16a162133c2657a2767448362a40f41777
Mihir Jain <63667062+mixh@users.noreply.github.com>
2023-11-18 19:40:01 -0500

    base files

---
+++ Captain.py
@@ -0,0 +1,16 @@
+from Creature import Creature
+
+
+class Captain(Creature):
+    def __init__(self, x, y):
+        super().__init__(x, y, 'V')
+        self._collected_veggies = []
+
+    def add_veggie(self, veggie):
+        self._collected_veggies.append(veggie)
+
+    def get_collected_veggies(self):
+        return self._collected_veggies
+
+    def set_collected_veggies(self, new_veggies):
+        self._collected_veggies = new_veggies
---
+++ Rabbit.py
@@ -0,0 +1,6 @@
+from Creature import Creature
+
+
+class Rabbit(Creature):
+    def __init__(self, x, y):
+        super().__init__(x, y, 'R')
